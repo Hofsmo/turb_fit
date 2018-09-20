264a755a2024e4e40b721b45f92fba2b05dcd09b
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-09-20 12:11:59 +0200

    Small changes before pull

--- create_G0.m
+++ create_G0.m
@@ -2,7 +2,7 @@ function G0 = create_G0(T1, T2, At, Tr, r, Tf, Tg, R, H, Kd)
 
 s = tf('s');
 Gt = At*(1-s*T1)/(1+s*T2);
-Gc = -(1+s*Tr)/(r*(1+s*Tf)*Tr*s+R*(1+s*Tr));
+Gc = (1+s*Tr)/(r*(1+s*Tf)*Tr*s+R*(1+s*Tr));
 Gs = 1/(1+s*Tg);
 Hj = 1/(2*H*s +Kd);
-G0 = -Hj/(1-Gt*Gc*Gs*Hj);
+G0 = -Hj/(1+Gt*Gc*Gs*Hj);
--- prepare_case.m
+++ prepare_case.m
@@ -1,14 +1,18 @@
-function [data, validation_data] = prepare_case(y, u, ratio, T, Ts )
+function [data, validation_data] = prepare_case(y, u, ratio, Ts )
 %PREPARE_CASE Summary of this function goes here
 %   Detailed explanation goes here
 
 yd = decimate(y,ratio);
-ud = decimate(u,ratio);
+n = size(u,2);
+ud = zeros(ceil(size(u,1)/ratio),n);
+for i = 1:n
+    ud(:,i) = decimate(u(:,i),ratio)';
+end
 Tsd = ratio*Ts;
-T1=T/2/Tsd+1;
+T1=floor(numel(yd)/2);
 
-data = iddata(yd,ud,Tsd);
-validation_data = iddata(yd(T1+1:end),ud(T1+1:end), Tsd);
+data = iddata(yd(1:T1,:),ud(1:T1,:),Tsd);
+validation_data = iddata(yd(T1+1:end,:),ud(T1+1:end,:), Tsd);
 
 data = detrend(data);
 validation_data = detrend(validation_data);
