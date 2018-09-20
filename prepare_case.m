2ac0c3b0490fe2de1f3fb5ee09f337ecbfddc010
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-09-20 12:08:43 +0200

    Fixed code for dataset length

--- prepare_case.m
+++ prepare_case.m
@@ -5,9 +5,9 @@ function [data, validation_data] = prepare_case(y, u, ratio, T, Ts )
 yd = decimate(y,ratio);
 ud = decimate(u,ratio);
 Tsd = ratio*Ts;
-T1=T/2/Tsd+1;
+T1=T/Tsd;
 
-data = iddata(yd,ud,Tsd);
+data = iddata(yd(1:T1),ud(1:T1),Tsd);
 validation_data = iddata(yd(T1+1:end),ud(T1+1:end), Tsd);
 
 data = detrend(data);
