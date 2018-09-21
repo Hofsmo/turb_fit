3258920db8065798d485969624aa813245507bfd
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-09-21 15:59:17 +0200

    Made functions for Knut's models

--- create_G0.m
+++ create_G0.m
@@ -1,7 +1,7 @@
-function G0 = create_G0(T1, T2, At, Tr, r, Tf, Tg, R, H, Kd)
+function G0 = create_G0(T1, T2, At, Tr, r, Tf, Tg, R, H, Kd,Dg)
 
 s = tf('s');
-Gt = At*(1-s*T1)/(1+s*T2);
+Gt = At*(1-s*T1)/(1+s*T2)-Dg;
 Gc = -(1+s*Tr)/(r*(1+s*Tf)*Tr*s+R*(1+s*Tr));
 Gs = 1/(1+s*Tg);
 Hj = 1/(2*H*s +Kd);
--- linearize_hygov.m
+++ linearize_hygov.m
@@ -1,5 +1,6 @@
-function [T1,T2,q0] = linearize_hygov(Pm0, Tw, qnl, At)
+function [T1,T2,Dg,q0] = linearize_hygov(Pm0, Tw, qnl, At,Dt)
 
 q0 = Pm0/At + qnl;
 T1 = (q0-qnl)*Tw;
 T2 = q0*Tw/2;
+Dg = q0*Dt;
