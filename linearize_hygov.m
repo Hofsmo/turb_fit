f394e9b8d55255792282c1f1871b147023c96016
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-09-21 16:59:49 +0200

    Commit before pull

--- linearize_hygov.m
+++ linearize_hygov.m
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-function [T1,q0] = linearize_hygov(Pm0, Tw, qnl, At)
-q0 = Pm0/At + qnl;
-T1 = q0*Tw;
+function [T1,T2,Dg,q0] = linearize_hygov(Pm0, Tw, qnl, At,Dt,w0)
 
-=======
-function [T1,T2,Dg,q0] = linearize_hygov(Pm0, Tw, qnl, At,Dt)
-
-q0 = Pm0/At + qnl;
+q0=(Pm0+(At*qnl))/(At-(w0*Dt));
 T1 = (q0-qnl)*Tw;
 T2 = q0*Tw/2;
 Dg = q0*Dt;
->>>>>>> 5f6207c125a483565e6a0c82342aac23b35fd6ab
+
